dc0881f5367021f5662eda6047c7b2481b3e75fb
Adrian Garcia <garcia.adrian@gmail.com>
2021-03-22 14:27:28 -0300

    fix environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  TIMELY_BASE_URL: 'https://timelyapp.time.ly/api/'
+  TIMELY_BASE_URL: 'https://timelyapp.time.ly/api/',
+  URL_CONFIG_PARAM: 'https://calendar.time.ly/ficceyp4'
 };
